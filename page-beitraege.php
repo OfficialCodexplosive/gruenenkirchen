c4f2c3983fba0ba6b75ff2466bbed0726fa4bbd0
OfficialCodexplosive <schoenau.julian@gmail.com>
2022-08-25 15:47:38 +0000

    add all_posts seciton

--- page-beitraege.php
+++ page-beitraege.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-  <main class="site-content">
+  <main class="site-content site-all">
     <div class="wrapper">
       <section class="all-posts">
         <?php 
@@ -13,45 +13,51 @@
             <h3>Alle Beiträge</h3>
             <div class="section">
             <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-            <a class="post" href="<?php the_permalink(); ?>">
-              <div class="img-wrapper">
-                <img src="<?php echo $image[0]; ?>"/>
-              </div>
-              <div class="post-overlay">
-                <div class="post-info">
-                  <!-- exclude all categories that are sub of 'position'-->
-                  <!--< ?php $categories = get_the_category(); ?>
-                  <h3>
-                    <ul class="post-categories">
-                      < ?php foreach($categories as $category){ ?>
-                        < ?php 
-                          $cat_parent_id = $category->category_parent;
-                          
-                          #$cat_parent = get_the_category_by_ID( $cat_parent_id );
-                          if( $cat_parent_id != 14 )
-                          {
+              
+              <a class="post" href="<?php the_permalink(); ?>">
+                <div class="post-overlay">
+                  <div class="post-info">
+                    <!-- exclude all categories that are sub of 'position'-->
+                    <!--< ?php $categories = get_the_category(); ?>
+                    <h3>
+                      <ul class="post-categories">
+                        < ?php foreach($categories as $category){ ?>
+                          < ?php 
+                            $cat_parent_id = $category->category_parent;
+                            
+                            #$cat_parent = get_the_category_by_ID( $cat_parent_id );
+                            if( $cat_parent_id != 14 )
+                            {
+                          ?>
+                          <li><a href="< ?php echo get_category_link($category->term_id);?>"><?php echo $category->name ?></a></li>
+                          < ?php } ?>
+                        < ?php }?>
+                      </ul>
+                    </h3>-->
+                    <h3><?php the_date(); ?></h3>
+                    <h2><?php the_title(); ?></h2>
+                    <p>
+                      <?php 
+                        $content = get_the_excerpt();
+                        echo wp_filter_nohtml_kses( $content );
                         ?>
-                        <li><a href="< ?php echo get_category_link($category->term_id);?>"><?php echo $category->name ?></a></li>
-                        < ?php } ?>
-                      < ?php }?>
-                    </ul>
-                  </h3>-->
-                  <h3><?php the_date(); ?></h3>
-                  <h2><?php the_title(); ?></h2>
-                  <p>
-                    <?php 
-                      $content = get_the_content();
-                      echo wp_filter_nohtml_kses( $content );
-                      ?>
-                  </p>
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </a>
+                <?php if (has_post_thumbnail( get_the_ID() ) ): ?>
+                  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'single-post-thumbnail' ); ?>
+                <?php else : ?>
+                  <?php $image = array(''); ?>
+                <?php endif; ?>
+                <!--<div class="img-wrapper" style="background-image: url('<?php echo $image[0]; ?>')">
+                </div>-->
+              </a>
             <?php endwhile; ?>
             </div>
+            <!---
             <div class="btn-wrap">
               <a class="call-to-action btn-more" href="./beitraege">Mehr</a>
-            </div>
+            </div>-->
           </div>
         </div>
             <?php wp_reset_postdata(); ?>
