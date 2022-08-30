e7af971183ce055e6c7f5569b4e0d80087976fe0
OfficialCodexplosive <schoenau.julian@gmail.com>
2022-08-30 14:11:15 +0000

    changes to vorstand

--- page-vorstand-child.php
+++ page-vorstand-child.php
@@ -1,8 +1,14 @@
 <?php get_header(); ?>
   <main class="site-content">
     <div class="wrapper">
-      <?php echo the_title(); ?>
-      <?php echo the_content(); ?>
-      <?php echo the_post_thumbnail_url(); ?>
+      <div id="vorstand-header">
+        <div class="column portrait">
+          <div id="vorstand-thumbnail" style="background-image:url('<?php echo the_post_thumbnail_url(); ?>');"></div>
+        </div>
+        <div class="column content">
+          <?php echo the_title(); ?>
+          <?php echo the_content(); ?>
+        </div>
+      </div>
     </div>
   </main>
