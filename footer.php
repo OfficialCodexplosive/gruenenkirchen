051a110162aea637315afc12b8b3615f6d607cf2
OfficialCodexplosive <schoenau.julian@gmail.com>
2022-09-01 13:06:29 +0000

    small changes to footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
    
     <footer class="site-footer">
       <div class="wrapper">
-        <span id="footer-title"><?php echo get_bloginfo( 'name' ); ?></span>
+        <!--<span id="footer-title">< ?php echo get_bloginfo( 'name' ); ?></span>-->
 
         <nav class="meta-nav">
         
@@ -11,15 +11,22 @@
                 'theme_location' => 'footer', 
                 'menu_class' => 'meta-nav-list' ) );
 
-        wp_nav_menu( 
-          array( 
-              'menu' => 'Unterfooter',
-              'menu_class' => 'sub-nav-list' ) );
+        
         ?>
         
         </nav>
 
       </div>
+      <div class="sub-footer">
+        <?php
+          wp_nav_menu( 
+            array( 
+                'menu' => 'Unterfooter',
+                'menu_class' => 'sub-nav-list' ) );
+        ?>
+      </div>
+
+      <div class="copyright-notice">Grünenkirchen Theme für <a href="https://wordpress.com/">WordPress</a></div>
     </footer>
   
     <?php wp_footer(); ?>
--- page-default.php
+++ page-default.php
@@ -7,6 +7,51 @@
                 <h3><?php echo the_title(); ?></h3>
                 <p><?php echo the_content(); ?></p>
             </div>
+            <?php
+                if($post->post_name != "themen")
+                {
+                  if ($post->post_parent)	{
+                    $ancestors=get_post_ancestors($post->ID);
+                    $root=count($ancestors)-2;
+                    $parent = $ancestors[$root];
+                    
+                  } else {
+                    $parent = $post->ID;
+                  }
+                  $parent_post = get_post($parent);
+
+                  $children = get_pages( array( 'child_of' => $parent_post->ID ) );
+
+                  $index = 0;
+                  $children_names = array();
+                  $downloads_page = NULL;
+                  foreach ( $children as $child )
+                  {
+                    $children_names[$index] = $child->post_name;
+                    
+                    if($child->post_name === "downloads")
+                    {
+                      $downloads_page = $child;
+                    }
+
+                    $index++;
+
+                  }
+
+                  if( in_array("downloads", $children_names) )
+                  {
+                  
+            ?>
+              <div class="center-section download-section">
+                <h3>Downloads</h3>
+                <?php
+                  echo $downloads_page->post_content;
+                ?>
+              </div>
+            <?php
+                }
+              }
+            ?>
         </div>
       </section>
     </div>
